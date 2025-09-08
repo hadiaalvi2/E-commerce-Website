de061b53fd246de29721a21fa061275d33e77309
hadiaalvi2 <hadia.alvi@zimogroup.org>
2025-09-08 11:19:17 +0500

    About page

---
+++ ecommerce-site/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import AboutPage from '../../components/about/AboutPage';
+import { useCart } from '@/context/CartProvider';
+import MainLayout from '@/components/layout/MainLayout';
+
+export const metadata = {
+  title: 'About Us - ShopHub',
+  description: 'Learn about ShopHub - your trusted partner in quality products and exceptional shopping experiences.',
+};
+
